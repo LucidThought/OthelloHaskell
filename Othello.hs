db47b37513345eb0d2e2a40fc1fd85e07f33575b
LucidThought <alata.it@gmail.com>
2015-01-30 16:48:46 -0700

    Added Cell magic code to Othello.hs

--- Othello.hs
+++ Othello.hs
@@ -116,3 +116,10 @@ replace xs n elem = let (ys,zs) = splitAt n xs
 replace2        :: [[a]] -> (Int,Int) -> a -> [[a]]
 replace2 xs (x,y) elem = replace xs y (replace (xs !! y) x elem)
 
+-- | Gets a cell in a row of cells
+getCell :: [a] -> Int -> Cell
+getCell xs n = xs !! n
+
+-- | Gets a cell from the board
+getCell2 :: [[a]] -> (Int,Int) -> Cell
+getCell2 xs (x,y) = getCell (xs !! y) x
