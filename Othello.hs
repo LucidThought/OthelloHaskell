37f625929f5f163fd287b8ba4db0132b3534bd02
rileylahd <rileylahd@gmail.com>
2015-01-30 16:54:35 -0700

    FIxed type error in Othello.hs

--- Othello.hs
+++ Othello.hs
@@ -117,9 +117,9 @@ replace2        :: [[a]] -> (Int,Int) -> a -> [[a]]
 replace2 xs (x,y) elem = replace xs y (replace (xs !! y) x elem)
 
 -- | Gets a cell in a row of cells
-getCell :: [a] -> Int -> Cell
+getCell :: [Cell] -> Int -> Cell
 getCell xs n = xs !! n
 
 -- | Gets a cell from the board
-getCell2 :: [[a]] -> (Int,Int) -> Cell
+getCell2 :: [[Cell]] -> (Int,Int) -> Cell
 getCell2 xs (x,y) = getCell (xs !! y) x
