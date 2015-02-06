202486a3b9392ddcab9a348385fdecea9e9897c7
LucidThought <alata.it@gmail.com>
2015-02-05 20:55:28 -0700

    Vertical flips now change pieces correctly when called from the 'flipThis' function

--- OthelloMoveCheck.hs
+++ OthelloMoveCheck.hs
@@ -401,13 +401,15 @@ counter45     [ [a0, a1, a2, a3, a4, a5, a6, a7],
 
 -- | The following flip functions are meant to flip pieces after making a valid play
 flipThis :: [[Cell]] -> Player -> (Int,Int) -> [[Cell]]
-flipThis board player (0,0) = flipRightForward (replace2a board (0,0) (tile player)) player (1,0)
-flipThis board player (7,0) = flipLeftForward (replace2a board (7,0) (tile player)) player (6,0)
-flipThis board player (0,7) = flipRightForward (replace2a board (0,7) (tile player)) player (1,7)
-flipThis board player (7,7) = flipLeftForward (replace2a board (7,7) (tile player)) player (6,7)
-flipThis board player (7,y) = flipLeftForward (replace2a board (7,y) (tile player)) player (6,y)
-flipThis board player (0,y) = flipRightForward (replace2a board (0,y) (tile player)) player (1,y)
-flipThis board player (j,i) = (flipLeftForward (flipRightForward (replace2a board (j,i) (tile player)) player (j+1,i)) player (j-1, i))
+flipThis board player (0,0) = (flipDownForward (flipRightForward (replace2a board (0,0) (tile player)) player (1,0)) player (0,1))
+flipThis board player (7,0) = (flipDownForward (flipLeftForward (replace2a board (7,0) (tile player)) player (6,0)) player (7,1))
+flipThis board player (0,7) = (flipUpForward (flipRightForward (replace2a board (0,7) (tile player)) player (1,7)) player (0,6))
+flipThis board player (7,7) = (flipUpForward (flipLeftForward (replace2a board (7,7) (tile player)) player (6,7)) player (7,6))
+flipThis board player (x,0) = (flipDownForward (flipRightForward (flipLeftForward (replace2a board (x,0) (tile player)) player (x-1,0)) player (x+1,0)) player (x,1))
+flipThis board player (x,7) = (flipUpForward (flipRightForward (flipLeftForward (replace2a board (x,0) (tile player)) player (x-1,0)) player (x+1,0)) player (x,6))
+flipThis board player (7,y) = (flipDownForward (flipUpForward (flipLeftForward (replace2a board (7,y) (tile player)) player (6,y)) player (7,y-1)) player (7, y+1))
+flipThis board player (0,y) = (flipDownForward (flipUpForward (flipRightForward (replace2a board (0,y) (tile player)) player (1,y)) player (0,y-1)) player (0, y+1))
+flipThis board player (x,y) = (flipDownForward (flipUpForward (flipLeftForward (flipRightForward (replace2a board (x,y) (tile player)) player (x+1,y)) player (x-1, y)) player (x,y-1)) player (x, y+1))
 
 
 flipRightForward :: Board -> Player -> (Int, Int) -> Board
