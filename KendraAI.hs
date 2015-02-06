0ad22c1c47b24371e8a29759d0f50f7d2ba966e7
Kendra Ann Wannamaker <kendrasemail@shaw.ca>
2015-02-06 15:18:48 -0700

    Maybe finished GreedyAI

--- KendraAI.hs
+++ KendraAI.hs
@@ -1,10 +1,37 @@
-isValid :: Board -> Player -> (Int, Int) -> Int -> Int
-isValidUp board player (x,y)
-			| hasAlly (getListUp (x,y) board) player == True = scoreUp (x,y) board player
-			| otherwise = 0
-			
-
 getList :: (Int, Int) -> Board -> Directions -> [Cell]
 getList ( x,y) board direction
-			| direction (x, y) == Nothing = []
+			| direction (x, y) == (-1,-1) = []
 			| otherwise = getCell (x,y) : getList (direction(x, y))
+
+cellValue :: (Int, Int) -> Player -> [[a]] -> Int
+cellValue (x,y) player board 
+
+			|getCell2 board (x, y) == tile (invertPlayer (player)) = 1
+			|getCell2 board (x, y) /= tile (invertPlayer (player)) = 0
+
+
+
+score :: (Int, Int) -> Board -> Player -> Int
+score (x, y) player = score' (x, y+1) board player Up 
+		    + score' (x+1, y+1) board player UpRight 
+		    + score' (x-1, y+1) board player UpLeft
+		    + score' (x+1, y) board player Right 
+		    + score' (x-1, y) board player Left
+		    + score' (x, y-1) board player Down
+		    + score' (x+1, y-1) board player DownRight
+		    + score' (x-1, y-1) board player DownLeft 
+		
+score' :: (Int, Int) -> [[a]] -> Player -> Directions -> Int
+score' (x, y) board player 
+			| ThisSpace (x,y) == (-1,-1) = 0
+			| direction (x,y) == (-1,-1) = 0
+			|(cellValue (x, y) player board) == 0 = 0
+			|(cellValue (x, y) player board) == 1 = 1 + (score' (direction (x, y)) board player)
+
+
+greedyPick :: Chooser
+greedyPick [] _ _ = Nothing
+greedyPick ( x:[] ) board cell = x
+greedyPick ( x:xs) board cell = max (score (x board playerOf (cell)) score (xs board playerOf (cell)))
+
+
--- Movement.hs
+++ Movement.hs
@@ -5,48 +5,63 @@ data Directions = Up
 				| Left
 				| Down
 				| DownLeft
-				| DownRight 
+				| DownRight
+				| ThisSpace
 				
- Up :: (Int, Int) -> Board -> Maybe (Int, Int)
+ Up :: (Int, Int) -> Board -> (Int, Int)
  Up ( _,y) board
-			| y+1 > 7 == True = Nothing
-			| otherwise = Just (x, y+1)
+			| y+1 > 7 == True = (-1,-1)
+			| otherwise = (x, y+1)
 			
- UpRight :: (Int, Int) -> Board -> Maybe (Int, Int)
+ UpRight :: (Int, Int) -> Board -> (Int, Int)
  UpRight ( x,y) board
-			| y+1 > 7 == True = Nothing
-			| x+1 > 7 == True = Nothing
-			| otherwise = Just(x+1, y+1)
+			| y+1 > 7 == True = (-1,-1)
+			| x+1 > 7 == True = (-1,-1)
+			| otherwise = (x+1, y+1)
 			
- UpLeft :: (Int, Int) -> Board -> Maybe (Int, Int)
+ UpLeft :: (Int, Int) -> Board -> (Int, Int)
  UpLeft ( x,y) board
-			| y+1 > 7 == True = Nothing
-			| x-1 < 0 == True = Nothing
-			| otherwise = Just(x-1, y+1)
+			| y+1 > 7 == True = (-1,-1)
+			| x-1 < 0 == True = (-1,-1)
+			| otherwise = (x-1, y+1)
 			
- Right :: (Int, Int) -> Board -> Maybe (Int, Int)
+ Right :: (Int, Int) -> Board -> (Int, Int)
  Right ( x,_) board
-			| x+1 > 7 == True = Nothing
-			| otherwise = Just(x+1, y)
+			| x+1 > 7 == True = (-1,-1)
+			| otherwise = (x+1, y)
 			
- Left :: (Int, Int) -> Board -> Maybe (Int, Int)
+ Left :: (Int, Int) -> Board -> (Int, Int)
  Left ( x,_) board
-			| x-1 < 0 == True = Nothing
-			| otherwise = Just(x-1, y)
+			| x-1 < 0 == True = (-1,-1)
+			| otherwise = (x-1, y)
 			
- Down :: (Int, Int) -> Board -> Maybe (Int, Int)
+ Down :: (Int, Int) -> Board -> (Int, Int)
  Down ( _,y) board
-			| y-1 < 0 == True = Nothing
-			| otherwise = Just(x, y-1)
+			| y-1 < 0 == True = (-1,-1)
+			| otherwise = (x, y-1)
 			
- DownRight :: (Int, Int) -> Board -> Maybe (Int, Int)
+ DownRight :: (Int, Int) -> Board -> (Int, Int)
  DownRight ( x,y) board
-			| y-1 < 0 == True = Nothing
-			| x+1 > 7 == True = Nothing
-			| otherwise = Just(x+1, y-1)
+			| y-1 < 0 == True = (-1,-1)
+			| x+1 > 7 == True = (-1,-1)
+			| otherwise = (x+1, y-1)
 			
- DownLeft :: (Int, Int) -> Board -> Maybe (Int, Int)
+ DownLeft :: (Int, Int) -> Board -> (Int, Int)
  DownLeft ( x,y) board
-			| y-1 < 0 == True = Nothing
-			| x-1 < 0 == True = Nothing
-			| otherwise = Just(x-1, y-1)
+			| y-1 < 0 == True = (-1,-1)
+			| x-1 < 0 == True = (-1,-1)
+			| otherwise = (x-1, y-1)
+
+ ThisSpace :: (Int, Int) -> Board -> (Int, Int)
+ ThisSpace ( x,y) board
+			| y+1 > 7 == True = (-1,-1)
+			| x+1 > 7 == True = (-1,-1)
+			| y-1 < 0 == True = (-1,-1)
+			| x-1 < 0 == True = (-1,-1)
+			| otherwise = (x, y+1)
+
+
+
+
+
+
