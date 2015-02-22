ea2d353ac418d9a01bdacb144285e998730656f2
Andrew <alata.it@gmail.com>
2015-02-21 18:22:20 -0700

    Delete CellMagic.hs
    Cleaning up temporary placeholder files

--- CellMagic.hs
+++
@@ -1,7 +0,0 @@
--- | Gets a cell in a row of cells
-getCell :: [a] -> Int -> Cell
-getCell xs n = xs !! n
-
--- | Gets a cell from the board
-getCell2 :: [[a]] -> (Int,Int) -> Cell
-getCell2 xs (x,y) = getCell (xs !! y) x
